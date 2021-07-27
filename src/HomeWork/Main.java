527e6bc66b4e8f0aecda58e8491631202c4b497a
Davidi4 <mrdavidsonoficial@gmail.com>
2021-07-27 20:53:27 +0300

    Initial commit

---
+++ src/HomeWork/Main.java
@@ -0,0 +1,18 @@
+package HomeWork;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        public static void printThreeWords
+
+            System.out.println("Orange");
+            System.out.println("Banana");
+            System.out.println("Apple");
+
+        }
+
+
+    }
+}
